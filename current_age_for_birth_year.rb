32cc13222e7e439ca30273f5e877757176933b77
Barak Saidoff <barak.saidoff@gmail.com>
2019-10-08 13:14:15 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,4 +1,7 @@
 def current_age_for_birth_year(year_born)
-  current_age = Time.now.year - year_born
-  current_age
+  #current_age = Time.now.year - year_born
+  #current_age
+
+  2003 - birth_year
+
 end
